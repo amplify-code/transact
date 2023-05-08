c634f37ca30fce0c0c254366e3573ea4b20caa9e
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2023-05-08 13:01:38 +0100

    Elements updates

--- resources/views/stripe/elements.blade.php
+++ resources/views/stripe/elements.blade.php
@@ -2,6 +2,20 @@
     <!--Stripe.js injects the Payment Element-->
 </div>
 
+@php
+
+    // $stripe = new \Stripe\StripeClient("sk_test_51IUCyCHZw0ztnS0JmrK5PZrebobxjxbsQ5nplzJD0hWvzh0dpxELT3Bpgctp6A130qQcifkwiRBBMq6iQAxBhfVa00qKcd0Yes");
+    // $intent = $stripe->paymentIntents->create(
+    //   [
+    //     'amount' => 1099,
+    //     'currency' => 'gbp',
+    //   ]
+    // );
+    // echo json_encode(array('client_secret' => $intent->client_secret));
+
+
+@endphp
+
 
 @push('scripts')
     @script('https://js.stripe.com/v3/')
@@ -10,14 +24,15 @@
         var stripe = Stripe('{{ config('transact.stripe_public_key') }}');
 
         var elements = stripe.elements({
+            {{-- // clientSecret: '{{ $intent->client_secret }}',--}}
             mode: 'payment',
             currency: 'gbp',
             amount: 1000,
         });
-
+        
         var paymentElement = elements.create('payment');
-
         paymentElement.mount("#payment-element");
+        
 
     </script>
 
