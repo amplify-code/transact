1a3ef0f386c4d2847fa17144d8b471f19b188c5d
Colin Cameron <colin@colincameron.net>
2025-11-27 15:02:17 +0000

    Update JS to handle setupintents

--- resources/views/stripe/elements.blade.php
+++ resources/views/stripe/elements.blade.php
@@ -12,10 +12,6 @@
     </div>
 </div>
 
-
-{{-- @dump($intent); --}}
-
-
 @push('styles')
     <style>
         #stripe-actions {
@@ -28,97 +24,48 @@
 @push('scripts')
     @scripttag('https://js.stripe.com/v3/')
     
-    <script>
+<script>
 
-        var stripe = Stripe('{{ config('transact.stripe_public_key') }}');
+    var stripe = Stripe('{{ config('transact.stripe_public_key') }}');
 
-        const options = {
+    const options = {
+        clientSecret: '{{ $intent->client_secret }}',
+    };
 
-            // for use with existing intent:
-            clientSecret: '{{ $intent->client_secret }}',
+    const intentType = '{{ get_class($intent) }}';
 
-            // Fully customizable with appearance API.
-            //  - Set via application.ini / Config, 
-            //  - Rendered as global js variables in StripeController::appearancejsAction
-            // fonts: stripe_fonts,
-            // appearance: {/*...*/
-                // variables: stripe_variables
-            // },
-        };
-
-    // Set up Stripe.js and Elements to use in checkout form, passing the client secret obtained in step 5
     const elements = stripe.elements(options);
 
-    // Create and mount the Payment Element
-    const paymentElement = elements.create('payment', {
+    const paymentElement = elements.create('payment', {});
 
-        @if($intent->payment_method_types)
-            paymentMethodOrder: {!! json_encode($transactable->getStripePaymentMethods()) !!},
-        @endif
-        
-    });
     paymentElement.mount('#payment-element');
 
     paymentElement.on('ready', function(event) {
-    // Handle ready event
         $('#stripe-actions').show();
+        console.log(intentType);
     });
-
-    // const form = document.getElementById('payment-form');
-
-    // form.addEventListener('submitx', async (event) => {
 
     $(document).on('click', '#stripe-elements-submit', async (event) => {
 
         event.preventDefault();
 
-        // Trigger form validation and wallet collection
-        const {error: submitError} = await elements.submit();
-        if (submitError) {
-            console.log(submitError);
-            // handleError(submitError);
-            return;
+        await elements.submit();
+
+        if (intentType == 'StripeSetupIntent') {
+            await stripe.confirmSetup({
+                elements,
+                confirmParams: {
+                    return_url: '{{ $return }}',
+                }
+            });
+        } else {
+            await stripe.confirmPayment({
+                elements,
+                confirmParams: {
+                    return_url: '{{ $return }}'
+                }
+            });
         }
-
-
-        // const {error, confirmationToken} = await stripe.createConfirmationToken({
-        //             elements,
-        //             params: {
-        //             shipping: {
-        //                 name: 'Jenny Rosen',
-        //                 address: {
-        //                 line1: '1234 Main Street',
-        //                 city: 'San Francisco',
-        //                 state: 'CA',
-        //                 country: 'US',
-        //                 postal_code: '94111',
-        //                 },
-        //             },
-        //             return_url: 'https://example.com/order/123/complete'
-        //             }
-        //         });
-
-        //     console.log('confirmationToken', confirmationToken);
-
-        const {error} = await stripe.confirmPayment({
-            //`Elements` instance that was used to create the Payment Element
-            elements,
-            confirmParams: {
-                return_url: '{{ $return }}'
-            }
-        });
-
-        // if (error) {
-        //     // This point will only be reached if there is an immediate error when
-        //     // confirming the payment. Show error to your customer (for example, payment
-        //     // details incomplete)
-        //     const messageContainer = document.querySelector('#error-message');
-        //     messageContainer.textContent = error.message;
-        // } else {
-        //     // Your customer will be redirected to your `return_url`. For some payment
-        //     // methods like iDEAL, your customer will be redirected to an intermediate
-        //     // site first to authorize the payment, then redirected to the `return_url`.
-        // }
     });
 
     </script>
